bc42ff2473bc9d870501f76a058aa22ef5840686
FIFSAK <anuar200572@gmail.com>
2023-09-14 15:43:46 +0600

    i

--- 1 lab/i.py
+++ 1 lab/i.py
@@ -1,1 +1,16 @@
-a = []
+n = int(input())
+s = input()
+i = 0
+flag = None
+while "S" in s == False or "K" in s == False:
+    if s[i] == "K" and flag == "s":
+        
+    if s[i] == "S":
+        flag = "s"
+    if s[i] == "K":
+        flag = "k"
+
+
+    if i==len(s)-1:
+        i = 0
+    i+=1
