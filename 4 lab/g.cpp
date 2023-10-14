88d3ac9e154820c5e20d464268ee1b1a2d4d9ded
FIFSAK <anuar200572@gmail.com>
2023-10-14 17:40:39 +0600

    finish

--- 4 lab/g.cpp
+++ 4 lab/g.cpp
@@ -1,76 +1,67 @@
 #include <iostream>
-#include <vector>
-#include <map>
 using namespace std;
 
 struct Node {
+    int val;
     Node* left;
     Node* right;
-    int key;
-    Node(int _key = 0) {
-        left = right = NULL;
-        key = _key;
-    }
+    Node(int v) : val(v), left(nullptr), right(nullptr) {}
 };
 
-struct BST {
+class BST {
+public:
     Node* root = nullptr;
-
-    void insert(int x) {
-        if (root == nullptr) {
-            root = new Node(x);
-            return;
-        }
-        insert(root, x);
+    
+    void insert(int val) {
+        root = insert(root, val);
     }
 
-    void insert(Node* cur, int x) {
-        if (cur->key >= x) {
-            if (cur->left == nullptr) {
-                cur->left = new Node(x);
-                return;
-            }
-            insert(cur->left, x);
-        } else {
-            if (cur->right == nullptr) {
-                cur->right = new Node(x);
-                return;
-            }
-            insert(cur->right, x);
+    int diameter() {
+        return diameter(root);
+    }
+private:
+    Node* insert(Node* node, int val) {
+        if (node == nullptr) {
+            return new Node(val);
         }
+        if (val < node->val) {
+            node->left = insert(node->left, val);
+        } else if (val > node->val) {
+            node->right = insert(node->right, val);
+        }
+        return node;
     }
 
-    int dist = 0;
+    int diameter(Node* node) {
+        if (node == nullptr) return 0;
 
-    int maxDepth(Node* cur) {
-        if (cur == nullptr) {
-            return 0;
-        }
+        int lHeight = height(node->left);
+        int rHeight = height(node->right);
 
-        int left = maxDepth(cur->left);
-        int right = maxDepth(cur->right);
-        
-        // Update the maximum diameter
-        dist = max(dist, left + right);
-        
-        return max(left, right) + 1;
+        int lDiameter = diameter(node->left);
+        int rDiameter = diameter(node->right);
+
+        return max(lHeight + rHeight + 1, max(lDiameter, rDiameter));
     }
 
-    int maxDepth() {
-        maxDepth(root);
-        return dist;
+    int height(Node* node) {
+        if (node == nullptr) return 0;
+        return 1 + max(height(node->left), height(node->right));
     }
 };
 
 int main() {
-    int n; cin >> n;
+    int N;
+    cin >> N;
+
     BST tree;
-    for (int i = 0; i < n; i++) {
-        int temp; cin >> temp;
-        tree.insert(temp);
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        tree.insert(x);
     }
-    int maxd = tree.maxDepth();
-    cout << maxd+1 << endl;
-    
+
+    cout << tree.diameter()  << endl; 
+
     return 0;
 }
