ff1c2b05e488d85f61e081d0ccc1708f723a8522
FIFSAK <anuar200572@gmail.com>
2023-10-24 22:50:50 +0600

    d finish all finish

--- 6 lab/d.cpp
+++ 6 lab/d.cpp
@@ -1,61 +1,107 @@
 #include <iostream>
-#include <map>
 #include <vector>
 #include <string>
-#include <sstream>
+#include <iomanip> 
+#include <algorithm>
+#include <map>
 
 using namespace std;
 
-// Function to parse a date in the format "dd-mm-yyyy"
-void parseDate(const string& date, int& day, int& month, int& year) {
-    stringstream ss(date);
-    char ignore;
+struct date {
+    int year, month, day;
+};
 
-    ss >> day >> ignore >> month >> ignore >> year;
+
+date parseDate(const string& dateString) {
+    stringstream ss(dateString);
+    string token;
+    date result;
+
+    getline(ss, token, '-');
+    result.day = stoi(token);
+
+    getline(ss, token, '-');
+    result.month = stoi(token);
+
+    getline(ss, token);
+    result.year = stoi(token);
+
+    return result;
 }
+
+bool compare(date a, date b) {
+    if (a.year != b.year) {
+        return a.year > b.year;
+    }
+    if (a.month != b.month) {
+        return a.month > b.month;
+    }
+    return a.day > b.day;
+}
+
+
+void heapifydown(vector<date>& dates, int n, int i) {
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && compare(dates[l], dates[largest])) {
+        largest = l;
+    }
+
+    if (r < n && compare(dates[r], dates[largest])) {
+        largest = r;
+    }
+
+    if (largest != i) {
+        swap(dates[i], dates[largest]);
+        heapifydown(dates, n, largest);
+    }
+}
+
+void buildHeap(vector<date>& dates, int n) {
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapifydown(dates, n, i);
+    }
+}
+
+void heapsort(vector<date>& dates) {
+    int n = dates.size();
+    buildHeap(dates, n);
+
+    for (int i = n - 1; i >= 0; i--) {
+        swap(dates[0], dates[i]);
+        heapifydown(dates, i, 0);
+    }
+}
+
 
 int main() {
-    int n; cin>>n;
-    // Sample list of dates
-    vector<string> dates;
-    for (int i = 0;i<n;i++){
+    long long n; cin >> n;
+    vector<date> dates;
+    
+    for (long long i = 0; i < n; i++) {
         string s;cin>>s;
-        dates.push_back(s);
+        date temp = parseDate(s);
+        dates.push_back(temp);
     }
-    // Map to store dates for sorting
-    map<int, map<int, int>> dateMap;
+    heapsort(dates);
 
-    // Populate the map
-    for (const auto& date : dates) {
-        int day, month, year;
-        parseDate(date, day, month, year);
-
-        dateMap[year][month] = day;  // Note: This will overwrite dates with the same year and month. If you expect multiple dates with the same year and month, you'd need a different approach.
-    }
-
-    // Print the sorted dates
-    for (const auto& yearEntry : dateMap) {
-    for (const auto& monthEntry : yearEntry.second) {
-        string dayStr, monthStr;
-
-        // Pad day if needed
-        if (monthEntry.second < 10) {
-            dayStr = "0" + to_string(monthEntry.second);
-        } else {
-            dayStr = to_string(monthEntry.second);
+    for(auto cur: dates){
+        if (cur.month<10){
+            if(cur.day<10){
+                cout<<'0'<<cur.day<<"-"<<'0'<<cur.month<<"-"<<cur.year<<endl;
+                continue;
+            }
+            cout<<cur.day<<"-"<<'0'<<cur.month<<"-"<<cur.year<<endl;
+            continue;
         }
-
-        // Pad month if needed
-        if (monthEntry.first < 10) {
-            monthStr = "0" + to_string(monthEntry.first);
-        } else {
-            monthStr = to_string(monthEntry.first);
-        }
-
-        // Print the formatted date
-        cout << dayStr << "-" << monthStr << "-" << yearEntry.first << endl;
+        if(cur.day<10){
+                cout<<'0'<<cur.day<<"-"<<cur.month<<"-"<<cur.year<<endl;
+                continue;
+            }
+        cout<<cur.day<<"-"<<cur.month<<"-"<<cur.year<<endl;
     }
-}
 
     return 0;
 }
